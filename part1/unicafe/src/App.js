4638b7b2950e138d0eba1f696dbfeb5ccd87836c
tylrk <tylerk1117@gmail.com>
2023-02-12 20:57:02 -0600

    completed step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,49 +1,52 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
+  return <button onClick={handleClick}>{text}</button>;
 };
 
-const Statistics = ({ good, neutral, bad, totalReviews }) => {
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
   return (
     <div>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p> all {totalReviews}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive * 100} %</p>
     </div>
-  )
-}
+  );
+};
 
-// UPDATE TOTAL REVIEWS BY SETTING NEW STATE 
+// UPDATE TOTAL REVIEWS BY SETTING NEW STATE
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+
   const handleGoodClick = () => {
     setGood(good + 1);
     console.log(good);
+
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
     console.log(neutral);
+
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
     console.log(bad);
+
   };
 
-  const totalReviews = () => {
-    return good + neutral + bad;
-  }
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = good / total;
 
   return (
     <div>
@@ -51,9 +54,16 @@ const App = () => {
       <Button handleClick={handleGoodClick} text="good" />
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} totalReviews={totalReviews}/>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
