a7b02c8818809e01c4ebf41a799c882d62d5f351
tylrk <tylerk1117@gmail.com>
2023-03-04 15:35:10 -0600

    Updates to API call

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [value, setValue] = useState('');
+  const [country, setCountry] = useState([]);
+  const [searchInput, setSearchInput] = useState(null);
+
+  useEffect(() => {
+    console.log('effect ran, country is now', value)
+
+    if(value) {
+      console.log('fetching country...')
+      axios.get(`https://restcountries.com/v3.1/name/${value}`)
+      .then(response => {
+        setCountry(response.data.map(item => item.name.common))
+      })
+    }
+  }, [value])
+
+  // const onSearch = (event) => {
+  //   event.preventDefault();
+  //   setSearchInput(value)
+  // }
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
   return (
     <div>
-      <p>find countries</p>
+      <br />
+      <form>
+      Find Countries: <input value={value} onChange={handleChange}/>
+      </form>
+      <pre>
+        {country.length > 0 && country[0]}
+      </pre>
     </div>
   );
 }
