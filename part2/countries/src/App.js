beecc82c17723272542084828ed29a5cdb7f8dea
tylrk <tylerk1117@gmail.com>
2023-03-08 22:35:23 -0600

    Almost finished with step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,21 +2,22 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [country, setCountry] = useState([]);
   const [searchInput, setSearchInput] = useState(null);
 
   useEffect(() => {
-    console.log('effect ran, country is now', value)
+    console.log("effect ran, country is now", value);
 
-    if(value) {
-      console.log('fetching country...')
-      axios.get(`https://restcountries.com/v3.1/name/${value}`)
-      .then(response => {
-        setCountry(response.data.map(item => item.name.common))
-      })
+    if (value) {
+      console.log("fetching country...");
+      axios
+        .get(`https://restcountries.com/v3.1/name/${value}`)
+        .then((response) => {
+          setCountry(response.data);
+        });
     }
-  }, [value])
+  }, [value]);
 
   // const onSearch = (event) => {
   //   event.preventDefault();
@@ -24,17 +25,30 @@ function App() {
   // }
 
   const handleChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return (
     <div>
       <br />
       <form>
-      Find Countries: <input value={value} onChange={handleChange}/>
+        Find Countries: <input value={value} onChange={handleChange} />
       </form>
       <pre>
-        {country.length > 0 && country[0]}
+        {country.length > 0 && country.length <= 10 ? (
+          country.map((item) => <p key={item.name.common}>{item.name.common}</p>)
+        ) : (
+          <p>Too many matches, please specify</p>
+        )}
+        {country.length === 1
+          ? country.map((item, idx) => (
+              <div key={idx}>
+                <p key={item.capital}>Capital: {item.capital}</p>
+                <p key={item.area}>Area: {item.area}</p>
+                <h3>Languages:</h3>
+              </div>
+            ))
+          : null}
       </pre>
     </div>
   );
