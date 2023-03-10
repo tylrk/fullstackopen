46847260d3cf5d5c825431044851b2f3f9c0a750
tylrk <tylerk1117@gmail.com>
2023-03-09 21:45:11 -0600

    step1 completed

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Country from "./components/Country";
 
 function App() {
   const [value, setValue] = useState("");
   const [country, setCountry] = useState([]);
-  const [searchInput, setSearchInput] = useState(null);
 
   useEffect(() => {
     console.log("effect ran, country is now", value);
@@ -19,11 +19,6 @@ function App() {
     }
   }, [value]);
 
-  // const onSearch = (event) => {
-  //   event.preventDefault();
-  //   setSearchInput(value)
-  // }
-
   const handleChange = (event) => {
     setValue(event.target.value);
   };
@@ -34,40 +29,9 @@ function App() {
       <form>
         Find Countries: <input value={value} onChange={handleChange} />
       </form>
-      <pre>
-        {country.length > 0 && country.length <= 10 ? (
-          country.map((item) => (
-            <h1 key={item.name.common}>{item.name.common}</h1>
-          ))
-        ) : (
-          <p>Too many matches, please specify</p>
-        )}
-        {country.length === 1 ? (
-          <div>
-            <p>Capital: {country[0].capital}</p>
-            <p>Area: {country[0].area}</p>
-            <h3>Languages:</h3>
-            <ul>
-            {country.map((item) =>
-              Object.values(item.languages).map((val) => (
-                <li key={val}>{val}</li>
-              ))
-            )}
-            </ul>
-            <img src={country[0].flags.png} alt={country[0].flags.alt}/>
-          </div>
-        ) : null}
-      </pre>
+      <Country country={country} />
     </div>
   );
 }
 
 export default App;
-
-// country.map((item, idx) => (
-//   <div key={idx}>
-//     <p key={item.capital}>Capital: {item.capital}</p>
-//     <p key={item.area}>Area: {item.area}</p>
-//     <h3>Languages:</h3>
-//   </div>
-// )
---
+++ part2/countries/src/components/Country.js
@@ -0,0 +1,28 @@
+const Country = ({ country }) => {
+  return (
+    <div>
+      {country.length > 1 && country.length <= 10
+        ? country.map((item) => (
+            <p key={item.name.common}>{item.name.common}</p>
+          ))
+        : null}
+      {country.length === 1 ? (
+        <div>
+          <h2>{country[0].name.common}</h2>
+          <p>Capital: {country[0].capital}</p>
+          <p>Area: {country[0].area}</p>
+          <h3>Languages:</h3>
+          <ul>
+            {Object.values(country[0].languages).map((val) => (
+              <li key={val}>{val}</li>
+            ))}
+          </ul>
+          <img src={country[0].flags.png} alt={country[0].flags.alt} />
+        </div>
+      ) : null}
+      {country.length > 10 ? <p>Too many matches, please specify</p> : null}
+    </div>
+  );
+};
+
+export default Country;
