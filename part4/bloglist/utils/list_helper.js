47bf9e6bec05b7412dbe1d6e7fe1b96a2ca93604
tylrk <tylerk1117@gmail.com>
2023-04-19 20:22:07 -0500

    completed part4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes);
+  const sum = likes.reduce((sum, likes) => sum + likes, 0);
+  return blogs.length === 0 ? 0 : sum;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
