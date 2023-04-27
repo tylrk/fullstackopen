8b74e3a9469eaec8a2dc8f80a34570e34da649a6
tylrk <tylerk1117@gmail.com>
2023-04-26 20:52:55 -0500

    completed part4.8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,23 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-    Blog.find({})
-      .then((blogs) => {
-        response.json(blogs);
-      })
-      .catch((err) => err.message);
-  });
-  
-  blogsRouter.post("/", (request, response) => {
-    const blog = new Blog(request.body);
-  
-    blog
-      .save()
-      .then((result) => {
-        response.status(201).json(result);
-      })
-      .catch((err) => err.message);
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
+
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
   });
 
-  module.exports = blogsRouter;
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
+
+module.exports = blogsRouter;
