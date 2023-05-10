fedde5b4b86dc6061920214ed16f6062b241e785
tylrk <tylerk1117@gmail.com>
2023-05-09 20:58:25 -0500

    completed part of part4.16 (need to add tests)

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,6 +10,20 @@ usersRouter.get("/", async (request, response) => {
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (!username) {
+    return response.status(400).json({ error: "Username missing" });
+  } else if (username.length < 3) {
+    return response
+      .status(400)
+      .json({ error: "Username must be 3 or more characters" });
+  } else if (!password) {
+    return response.status(400).json({ error: "Password missing" });
+  } else if (password.length < 3) {
+    return response
+      .status(400)
+      .json({ error: "Password must be 3 or more characters" });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
--- part4/bloglist/tests/listHelper.test.js
+++ part4/bloglist/tests/listHelper.test.js
@@ -209,59 +209,38 @@ describe("most total likes", () => {
   });
 });
 
-describe('when there is initially one user in db', () => {
+describe("when there is initially one user in db", () => {
   beforeEach(async () => {
-    await User.deleteMany({})
+    await User.deleteMany({});
 
-    const passwordHash = await bcrypt.hash('sekret', 10)
-    const user = new User({ username: 'root', passwordHash })
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
 
-    await user.save()
-  })
+    await user.save();
+  });
 
-  test('creation succeeds with a fresh username', async () => {
-    const usersAtStart = await listHelper.usersInDb()
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await listHelper.usersInDb();
 
     const newUser = {
-      username: 'tylr',
-      name: 'Tyler Khan',
-      password: 'test',
-    }
+      username: "tylr",
+      name: "Tyler Khan",
+      password: "test",
+    };
 
     await api
-      .post('/api/users')
+      .post("/api/users")
       .send(newUser)
       .expect(201)
-      .expect('Content-Type', /application\/json/)
+      .expect("Content-Type", /application\/json/);
 
-    const usersAtEnd = await listHelper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+    const usersAtEnd = await listHelper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
 
-    const usernames = usersAtEnd.map(u => u.username)
-    expect(usernames).toContain(newUser.username)
-  })
-
-  test('creation fails with proper statuscode and message if username already taken', async () => {
-    const usersAtStart = await listHelper.usersInDb()
-
-    const newUser = {
-      username: 'root',
-      name: 'user',
-      password: 'test2',
-    }
-
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-
-    expect(result.body.error).toContain('expected `username` to be unique')
-
-    const usersAtEnd = await listHelper.usersInDb()
-    expect(usersAtEnd).toEqual(usersAtStart)
-  })
-})
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
