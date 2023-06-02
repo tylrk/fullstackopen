d72a482961d5b95606552cde6bd963e48e0e44ac
tylrk <tylerk1117@gmail.com>
2023-06-02 18:58:51 -0500

    completed part5.10

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -131,9 +131,11 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Toggle>
       <br />
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} user={user} like={addLikes} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} user={user} like={addLikes} />
+        ))}
     </div>
   );
 };
