d66bda8a56257024d052b6082acd5d52f19aa8b3
tylrk <tylerk1117@gmail.com>
2023-09-07 23:01:01 -0500

    completed part 6.22

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,11 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { getAnecdotes } from "./requests";
+import { getAnecdotes, voteOnAnecdote } from "./requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const voteMutation = useMutation(voteOnAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
   const handleVote = (anecdote) => {
-    console.log("vote");
+    voteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
 
   const result = useQuery({
